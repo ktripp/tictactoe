3d648226b49c3db23f679cb298cee5aac19c086d
Kelsey Tripp <kelsey.tripp@netapp.com>
2015-03-25 17:58:34 -0700

    Refactored and added documentation

--- app/tictactoe.py
+++ app/tictactoe.py
@@ -2,16 +2,27 @@
 
 from copy import deepcopy
 
-# constants
+# symbol constants
 EX = "ex"
 OH = "oh"
 TIE = "tie"
 
 def getOppositePlayer(player):
+    """
+    Given a player EX or OH, returns the alternate player.
+    """
     return OH if player == EX else EX
 
 class TicTacToe:
-    def __init__(self, human):
+    """
+    Represents a Tic Tac Toe game.
+    """
+    def __init__(self, human=EX):
+        """
+        Creates a new Tic Tac Toc game with a 3x3 board.
+        The human player will by default be given "X" unless otherwise specified.
+        The game keeps track of the row, column, and player of the last move made.
+        """
         assert human == EX or human == OH
         self.board = [[None, None, None],
                       [None, None, None],
@@ -19,15 +30,27 @@ class TicTacToe:
         self.lastMove = None
         self.human = human
         self.computer = getOppositePlayer(human)
+        self.firstMove = True
 
     def __str__(self):
+        """
+        Prints out a representation of the board
+        """
         return "\n".join("".join([self._formatItem(item) for item in row])
             for row in self.board)
 
     def _formatItem(self, item):
-        return '{:^3}'.format(item) if item is not None else " - "
+        """
+        Internal helper function for printing.
+        """
+        printableValue = "X"
+        if item == OH: printableValue = "O"
+        return '{:^3}'.format(printableValue) if item is not None else " - "
 
     def move(self, row, col, player=None):
+        """
+        Places a player's symbol on the board at the given row and column.
+        """
         if player is None: player = self.human
         assert row >= 0 and row < 3
         assert col >= 0 and col < 3
@@ -35,61 +58,97 @@ class TicTacToe:
         self.lastMove = (row, col, player)
 
     def respondToMove(self, player=None):
+        """
+        Responds to the last move by using the minimax algorithm to choose
+        the best spot to play, and returns the row and column of that spot.
+        """
         if player is None: player = self.computer
-        loc = findNextMove(self, player)
+        loc = Minimax.findNextMove(self, player)
         if loc is None:
             return None
         row, col = loc
         self.move(row, col, player)
         return row, col
 
-    def checkLine(self, line):
+    def checkForWin(self, line):
+        """
+        Determines whether or not a list of three symbols
+        is a winning line (all three symbols are the same).
+        """
         return None not in line and len(set(line)) == 1
 
     def isInDiagonal(self, row, col):
+        """
+        Determines whether or not a given location is part
+        of a diagonal line (i.e. a corner or a middle spot).
+        """
         return row == col or row + col == 2
 
     def checkDiagonals(self):
+        """
+        Checks the diagonal lines for a win.
+        Returns a list of spot coordinates if the diagonal is a win.
+        """
         left = [(0, 0), (1, 1), (2, 2)]
         right = [(0, 2), (1, 1), (2, 0)]
-        if self.checkLine([self.board[r][c] for r, c in left]): return left
-        if self.checkLine([self.board[r][c] for r, c in right]): return right
+        if self.checkForWin([self.board[r][c] for r, c in left]): return left
+        if self.checkForWin([self.board[r][c] for r, c in right]): return right
         return None
  
     def isOver(self):
+        """
+        Determines whether or not the game is over (a tie or a win).
+        Returns a tuple representing the state of game:
+            (isOver, symbol, [locations])
+                isOver - True if the game is a win or a tie, False otherwise
+                symbol - the symbol of the winning player, or TIE if it is a tie
+                locations - a list of tuples of rows and columns of the winning squares
+        We only have to check the lines containing the location of the last move.
+        """
+        # if this is the first move we aren't done yet!
         if self.lastMove is None: return False, None, None
 
+        # get the last move and player that did it
         (r, c, winner) = self.lastMove
-        # check row
+        # check row containing last move
         row = self.board[r]
-        if self.checkLine(row):
+        if self.checkForWin(row):
             return True, winner, [(r, i) for i in range(len(row))]
 
-        # check col
+        # check column containing last move
         col = zip(*self.board)[c]
-        if self.checkLine(col):
+        if self.checkForWin(col):
             return True, winner, [(i, c) for i in range(len(col))]
 
-        # check diagonals
+        # check diagonals containing last move
         if self.isInDiagonal(r, c):
             diag = self.checkDiagonals()
             if diag is not None:
                 return True, winner, diag
 
-        # check for tie
+        # check for tie - if all squares are filled
         if None not in [col for row in self.board for col in row]:
             return True, TIE, None
         
-        # if we haven't won or tied we aren't done
+        # if we haven't won or tied, we aren't done
         return False, None, None
 
     def getGameState(self):
+        """
+        Returns a dictionary representing the current state of the game.
+            gameOver - True if the game is a win or a tie, False otherwise
+            winner - the symbol of the winning player (EX or OH), TIE if it is a tie, else None
+            winLine - a list of tuples of rows and columns of the winning squares, if a win
+        """
         (isOver, winner, line) = self.isOver()
         return {"gameOver": isOver,
                 "winner": winner,
                 "winLine": line}
 
     def getAvailableMoves(self):
+        """
+        Returns a list of tuples representing squares that are not yet taken.
+        """
         moves = []
         for r in range(len(self.board)):
             row = self.board[r]
@@ -98,84 +157,79 @@ class TicTacToe:
                     moves.append((r, c))
         return moves
 
+class Minimax:
+    """
+    Utility class to run the minimax algorithm to help not lose tic tac toe!
+    """
+    @staticmethod
+    def findNextMove(game, player):
+        """
+        Uses the minimax algorithm to find the best next move
+        for a given TicTacToe game for a given player.
+        Returns a tuple with the row and column of the next move.
 
-def findNextMove(board, player):
-    potentialMoves = board.getAvailableMoves()
-    maxScore = -1
-    maxMove = None
+        game - a TicTacToe game in any state of play
+        player - the symbol of the player that wants to find a move
+        """
+        # get a list of potential squares we can move
+        potentialMoves = game.getAvailableMoves()
+        maxScore = -1
+        maxMove = None
 
-    for row, col in potentialMoves:
-        currBoard = deepcopy(board)
-        currBoard.move(row, col, player)
-        currScore = minimax(currBoard, getOppositePlayer(player))
-        if currScore >= maxScore:
-            maxScore = currScore
-            maxMove = (row, col)
+        # run the minimax algorithm to determine if a potential move is the best move
+        for row, col in potentialMoves:
+            # copy the game and make the move
+            currBoard = deepcopy(game)
+            currBoard.move(row, col, player)
+            # get the score for this move
+            currScore = Minimax._minimax(currBoard, getOppositePlayer(player))
+            # track the best score and move
+            if currScore >= maxScore:
+                maxScore = currScore
+                maxMove = (row, col)
 
-    return maxMove
+        # return the best potential move
+        return maxMove
 
+    @staticmethod
+    def _minimax(game, player, computer=OH):
+        """
+        Runs the minimax algorithm with a given TicTacToe board,
+        the symbol of a given player, and the symbol of the computer player.
+        Future implementations should use alpha-beta pruning to speed things up!
 
-def minimax(board, player, computer=OH):
-    multiplier = 1 if computer == player else -1
+        game - a TicTacToe game in any state of play
+        player - the symbol of the player that wants to find a move
+        computer - the symbol of the computer (the one we want to win)!
+        """
+        # weight the computer positively and the human player negatively
+        multiplier = 1 if computer == player else -1
 
-    (isOver, winner, _) = board.isOver()
-    if isOver: return getScore(winner)
-    
-    potentialMoves = board.getAvailableMoves()
-    maxScore = -1
-    for row, col in potentialMoves:
-        currBoard = deepcopy(board)
-        currBoard.move(row, col, player)
-        currScore = multiplier * minimax(currBoard, getOppositePlayer(player))
-        if currScore >= maxScore:
-            maxScore = currScore
+        # if the game is over return the score
+        (isOver, winner, _) = game.isOver()
+        if isOver: return Minimax._getScore(winner)
+        
+        # otherwise iterate through potential moves
+        potentialMoves = game.getAvailableMoves()
+        maxScore = -1
+        for row, col in potentialMoves:
+            currBoard = deepcopy(game)
+            currBoard.move(row, col, player)
+            # recursively call the function to find the best score
+            currScore = multiplier * Minimax._minimax(currBoard, getOppositePlayer(player))
+            if currScore >= maxScore:
+                maxScore = currScore
 
-    return multiplier * maxScore
+        # return the score weighted appropriately
+        return multiplier * maxScore
 
-
-def getScore(winner, computer=OH):
-    if winner is None or winner is TIE:
-        return 0
-    else:
-        return 1 if winner == computer else -1
-
-
-
-# t = TicTacToe(EX)
-# t.move(0, 0, EX)
-# print t
-# print t.getGameState()
-# print "\n"
-
-# t.respondToMove(OH)
-# print t
-# print t.getGameState()
-# print "\n"
-
-# row, col = findNextMove(t, OH)
-# t.move(row, col, OH)
-# print t
-# print t.getGameState()
-# print "\n"
-
-# t.move(1, 0, EX)
-# print t
-# print t.getGameState()
-# print "\n"
-
-# row, col = findNextMove(t, OH)
-# t.move(row, col, OH)
-# print t
-# print t.getGameState()
-# print "\n"
-
-# t.move(2, 1, EX)
-# print t
-# print t.getGameState()
-# print "\n"
-
-# row, col = findNextMove(t, OH)
-# t.move(row, col, OH)
-# print t
-# print t.getGameState()
-# print "\n"
+    @staticmethod
+    def _getScore(winner, computer=OH):
+        """
+        Returns the score based on the symbol of the winner and the
+        symbol of the computer, or tie.  
+        """
+        if winner is None or winner is TIE:
+            return 0
+        else:
+            return 1 if winner == computer else -1
